45bd9912c8337e0543c09bb8274688e0cf3811ee
Benjamin Krajancic <bkrajancic@Benjamins-MacBook-Pro.local>
2020-12-18 17:25:44 +1100

    Fix scripts which do not work on older version of docker.

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
-name=fld-bot
-docker stop ${name}
-docker rm ${name}
+#!/bin/bash
 
-tag=fld-bot
-docker rmi ${tag}
-docker build -t ${tag} . -f dockerfile
+# By appending "|| true" execution is allowed to continue even when 0 isn't returned.
+export name=fld-bot
+docker stop ${name} || true
+docker rm ${name} || true
+
+export tag=fld-bot
+docker rmi ${tag} || true
+docker build -t ${tag} -f dockerfile .
 
 docker run --name ${name} -d ${tag}
--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
-tag=fld-bot-test
-docker build --force-rm -t ${tag} . -f dockerfile.test
+#!/bin/bash
+
+export tag=fld-bot-test
+docker build --force-rm -t ${tag} -f dockerfile.test .
 docker run --rm ${tag} 
 docker rmi ${tag}
