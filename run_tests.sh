67f4e78e77a451dbd5e5931d99b1d11cce2075e0
Benjamin Krajancic <15332488+BKrajancic@users.noreply.github.com>
2021-01-12 06:24:15 +1100

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+    echo "A single parameter must be passed, which becomes the container's name."
+    exit 1
+fi
+
 export tag=latest
-export name=fld-bot-test
+export name=${1}
 docker build --force-rm -t ${name}:${tag} -f dockerfile.test .
 docker run --rm ${name}:${tag}
 result=$?
