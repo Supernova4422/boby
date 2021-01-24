ae4371af0152357ebfcf8327d4aa093d480fef2b
Benjamin Krajancic <bkrajancic@Benjamins-MacBook-Pro.local>
2021-01-24 16:44:54 +1100

    Remove go lint scripts, because there is no benefit to using a container.

--- run_lint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export tag=latest
-export name=boby-lint
-docker build --force-rm -t ${name}:${tag} -f dockerfile.lint .
-docker run --rm ${name}:${tag}
-result=$?
-docker rmi ${name}:${tag}
-exit $result
