c503ef24a89754257eb60adc38ac87bdb5ab4a8a
Benjamin Krajancic <ben.k99@outlook.com.au>
2021-11-11 21:44:29 +0000

    Forgot to push these files

--- src/service/message.go
+++ src/service/message.go
@@ -1,11 +1,14 @@
 package service
 
+import "image/draw"
+
 // A Message is sent using a Sender.
 type Message struct {
 	URL         string
 	Title       string
 	Description string
 	Fields      []MessageField
+	Image       draw.Image
 }
 
 // A MessageField stores a field and value pair.
