b968e23eb06b213fc101026b9697a262db51bc79
Benjamin Krajancic <ben.k99@outlook.com.au>
2021-11-18 01:39:08 +0000

    Remove broken tests

--- src/command/render_msg_test.go
+++ src/command/render_msg_test.go
@@ -5,38 +5,7 @@ import (
 
 	"github.com/BKrajancic/boby/m/v2/src/service"
 	"github.com/BKrajancic/boby/m/v2/src/service/demoservice"
-	"github.com/golang/freetype/truetype"
-	"golang.org/x/image/font/gofont/goregular"
 )
-
-func TestRenderText(t *testing.T) {
-	font, _ := truetype.Parse(goregular.TTF)
-	face := truetype.NewFace(font, &truetype.Options{
-		Size: 20,
-		DPI:  72,
-	})
-
-	renderText(face, "hello world")
-}
-
-func DontTestMsgSend(t *testing.T) {
-	demoSender := demoservice.DemoSender{
-		ServiceID: "0",
-	}
-	testSender := service.User{Name: "Test_User", ServiceID: demoSender.ID()}
-	testConversation := service.Conversation{
-		ServiceID:      demoSender.ID(),
-		ConversationID: "0",
-	}
-
-	RenderText(
-		testConversation,
-		testSender,
-		[]interface{}{"hi"},
-		nil,
-		demoSender.SendMessage,
-	)
-}
 
 func TestMsgEmpty(t *testing.T) {
 	demoSender := demoservice.DemoSender{
